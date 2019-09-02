77fdfd62d85b459930f0d80df5b2db9ab46c8caf
Terrance <Tr@Terrances-MacBook-Pro.local>
2019-09-02 17:40:47 +0800

    using cors

--- functions/gtrans.js
+++ functions/gtrans.js
@@ -13,6 +13,12 @@ exports.handler = async event => {
   console.log("trans result:", r)
   return {
     statusCode: 200,
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': 'true'
+    },
     body: r,
   };
 
