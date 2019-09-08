946da6aa447d6815f3ab191f7c394317a8018011
Terrance <Tr@Terrances-MacBook-Pro.local>
2019-09-08 14:22:07 +0800

    import engines data to faunadb

---
+++ migrations/1567915539171_import_engines_data.js
@@ -0,0 +1,9 @@
+var enginesData = require('../config/engine-config.json')
+
+module.exports.up = (q) => {
+  return q.Map(enginesData, q.Lambda('engine', q.Create(q.Collection('Engines'), { data: q.Var('engine') })))
+}
+
+module.exports.down = (q) => {
+  return q.Map(q.Paginate(q.Match(q.Index('all_Engines'))), q.Lambda('engine', q.Delete(q.Var('engine'))))
+}
