2a53da6fa046c36c990fe13074d635a95e334f6a
Terrance <Tr@Terrances-MacBook-Pro.local>
2019-09-08 14:22:45 +0800

    add function for get engines

---
+++ src/functions/engines-get-all.js
@@ -0,0 +1,27 @@
+import faunadb from 'faunadb'
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET
+})
+
+exports.handler = (event, context, callback) => {
+  var pageHelper = client.paginate(q.Match(q.Index('all_Engines')))
+
+  var r = pageHelper
+    .map((ref) => q.Get(ref))
+    .each((data) => {
+      var response = data.map((_) => _.data)
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      })
+    })
+    .catch((error) => {
+      console.log('error', error)
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      })
+    })
+}
